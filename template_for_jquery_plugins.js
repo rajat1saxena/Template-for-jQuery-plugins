756904e52f0bf4165e749a5e8a224f55322bf6bf
Rajat Saxena <rajat.saxena.work@gmail.com>
2012-08-19 00:19:52 +0530

    Template for jQuery plugins

---
+++ template_for_jquery_plugins.js
@@ -0,0 +1,34 @@
+/*
+* <Plugin name>
+* <Short description of functionalities of the plugin>
+* 
+* Copywrite (c) <Year> <Your name>(<Your email>)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+/*
+* Note: replace 'PLUGIN_NAME' with the name of your plugin
+*/
+(function($){
+	$.fn.PLUGIN_NAME = function(){
+		return this.each(function(){
+
+			/*plugin code starts here*/
+			
+			/*plugin code ends here*/
+
+		});
+	}
+})(jQuery);
